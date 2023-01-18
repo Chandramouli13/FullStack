0df2b8a32ab3b47b682d42ff93ba90df4497b13a
Chandramouli13 <chandurajesh13@gmail.com>
2023-01-18 21:30:00 +0530

    first commit

--- React Edureka/routing6/src/index.js
+++ React Edureka/routing6/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './component/App';
+import ReactDOM from 'react-dom';
+import Routing from './component/Routing';
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-root.render(<App/>);
+root.render(<Routing/>)
